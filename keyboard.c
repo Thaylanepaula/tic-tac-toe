8b003623d065d3f8fe637db07ec9590fcc55e840
Thaylanepaula <151591263+Thaylanepaula@users.noreply.github.com>
2024-11-01 13:24:26 -0300

    Update keyboard.c

--- keyboard.c
+++ keyboard.c
@@ -1,9 +1,9 @@
-#include <stdio.h>
 #include "keyboard.h"
+#include "cli.h"  
 
 int getUserGuess() {
     int guess;
-    printf("Digite seu palpite (entre 1 e 100): ");
-    scanf("%d", &guess);
+    cli_input("Digite seu palpite (entre 1 e 100): ", &guess); 
     return guess;
 }
+
